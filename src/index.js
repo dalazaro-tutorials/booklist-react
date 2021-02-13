1115ab48dd582dd888bd4ca2b79b5a51ffb69767
Daryl Jason Lazaro <daryl@docspera.com>
2021-02-13 01:02:20 -0800

    Cleaned boilerplate, added nested components rendering props from local JSON obj

--- src/index.js
+++ src/index.js
@@ -1,17 +1,100 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// include CSS
+import './index.css'
+
+// JSON Object
+const responseArray = [
+	{
+		"url": "https://images-na.ssl-images-amazon.com/images/I/51iDWMHbdhL._SX258_BO1,204,203,200_.jpg",
+		"title": "Magnolia Table",
+		"author": "Joanna Gaines"
+	},
+	{
+		"url": "https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg",
+		"title": "A Promised Land",
+		"author": "Barack Obama"
+	},
+	{
+		"url": "https://images-na.ssl-images-amazon.com/images/I/51NYjNgldSL._SX330_BO1,204,203,200_.jpg",
+		"title": "Where the Crawdads Sing",
+		"author": "Delia Owens"
+	},
+	{
+		"url": "https://images-na.ssl-images-amazon.com/images/I/51MzSX1B7RL._SX412_BO1,204,203,200_.jpg",
+		"title": "The 50 States: Explore the U.S.A. with 50 fact-filled maps!"
+	},
+	{
+		"title": "Becoming",
+		"author": "Michelle Obama"
+	},
+	{
+		"url": "https://images-na.ssl-images-amazon.com/images/I/511pAnqlhML._SX474_BO1,204,203,200_.jpg",
+		"title": "The Wonderful Things You Will Be",
+		"author": "Emily Winfield Martin"
+	}
+]
+
+// Component as traditional function declaration
+function BookList() {
+	return (
+		// For any adjacent JSX tags, wrap them in a <React.Fragment> tag instead of <div> (or else you will end up with div soup)
+		<React.Fragment>
+			<h2>Book List</h2>
+			<section className="booklist">
+				{responseArray.map((element, index) => {
+					return <Book key={index} imageUrl={element.url} title={element.title} authorName={element.author} />
+				})}
+			</section>
+		</React.Fragment>
+	);
+}
+
+// Component as arrow function
+const Book = (props) => {
+	console.log(props);
+	const altText = (props.title && props.authorName) ? props.title.toLowerCase().split(' ').join('_') + '_' + props.authorName.toLowerCase().split(' ').join('_') : '';
+	return (
+		<div className="book">
+			<Cover imageUrl={props.imageUrl} alt={altText} />
+			<Title title={props.title} />
+			<Author authorName={props.authorName} />
+		</div>
+	);
+}
+
+const Cover = (props) => {
+	const imageUrl = (props.imageUrl) ? props.imageUrl : '',
+	altText = (props.altText) ? props.altText : 'book_cover';
+	return <img src={imageUrl} alt={altText}/>
+}
+
+
+const Title = (props) => {
+	const title = (props.title) ? props.title.toUpperCase() : 'Unknown Title';
+	return <h3>{title}</h3>;
+}
+
+
+// style needs double brackets: outer brackets are JavaScript world, inner brackets are an object
+const Author = (props) => {
+	const authorName = (props.authorName) ? props.authorName : 'Unknown Author';
+	return (
+		<h4 style={{
+			// 'color' or color
+			color: '#617d98',
+			// 'font-size' or fontSize
+			fontSize: '0.75rem',
+			marginTop: '0.25rem'
+		}}>
+			{/* authorName string interpolated */}
+			by {authorName}
+		</h4>
+	);
+}
+
+ReactDom.render(
+	<BookList />,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
